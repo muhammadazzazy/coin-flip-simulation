e1ce3123e8b2f731a3ff124ebe3967f6b994d6e8
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-13 16:28:53 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import random
+
+
+def main() -> None:
+    outcomes = ['head', 'tail']
+    counts = {outcomes[0]: 0, outcomes[1]: 0}
+    print('Welcome to the 🪙 Flip Simulator!')
+    while True:
+        user_input = input('Flip a coin? (Y/n) ')
+        if user_input.lower() == 'y':
+            outcome: str = random.choice(outcomes)
+            print(f'{outcome.capitalize()}')
+            if outcome == 'head':
+                counts[outcomes[0]] += 1
+            else:
+                counts[outcomes[1]] += 1
+        else:
+            print(f'Number of heads: {counts[outcomes[0]]}')
+            print(f'Number of tails: {counts[outcomes[1]]}')
+            print(
+                f'Number of coin flips: {counts[outcomes[0]]+counts[outcomes[1]]}')
+            print('Thanks for trying my program!')
+            exit()
+
+
+if __name__ == '__main__':
+    main()
